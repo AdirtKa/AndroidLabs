44bc478895d31c142c0fa3a4c29ed16befeac872
AdirtKa <AdirtKa@yandex.ru>
2025-09-07 21:02:12 +1000

    .

--- ThirdLab/app/src/main/java/com/example/thirdlab/MainActivity.kt
+++ ThirdLab/app/src/main/java/com/example/thirdlab/MainActivity.kt
@@ -1,20 +1,31 @@
 package com.example.thirdlab
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.thirdlab.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    companion object {
+        const val EXTRA_USER_TEXT = "EXTRA_USER_TEXT"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Кнопка "Перейти на 2"
+        binding.btnNext.setOnClickListener {
+            val text = binding.etInput.text?.toString().orEmpty()
+            val intent = Intent(this, SecondActivity::class.java).apply {
+                putExtra(EXTRA_USER_TEXT, text)
+            }
+            startActivity(intent)
         }
     }
 }
---
+++ ThirdLab/app/src/main/java/com/example/thirdlab/SecondActivity.kt
@@ -0,0 +1,25 @@
+package com.example.thirdlab
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.thirdlab.databinding.ActivitySecondBinding
+
+class SecondActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySecondBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val received = intent.getStringExtra(MainActivity.EXTRA_USER_TEXT).orEmpty()
+        binding.tvResult.text = if (received.isBlank()) "(пусто)" else received
+
+        // Кнопка "назад"
+        binding.btnBack.setOnClickListener {
+            finish() // вернёт на 1 activity
+        }
+    }
+}
