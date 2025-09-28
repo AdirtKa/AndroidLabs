eeaa5fb5120f2dd2bd7f7b6ba151caf09d2021c0
AdirtKa <AdirtKa@yandex.ru>
2025-09-28 22:26:56 +1000

    add bounce effect

--- FifthLab/app/src/main/java/com/example/fifthlab/TweetListScreen.kt
+++ FifthLab/app/src/main/java/com/example/fifthlab/TweetListScreen.kt
@@ -8,7 +8,9 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
@@ -29,8 +31,12 @@ fun TweetListScreen() {
         Tweet(R.drawable.avatar2, "Список работает как твиттер!")
     )
 
+    val scrollState = rememberScrollState()
+
     LazyColumn(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(scrollState), // Добавляем bounce эффект
         contentPadding = PaddingValues(8.dp)
     ) {
         items(tweets) { tweet ->
